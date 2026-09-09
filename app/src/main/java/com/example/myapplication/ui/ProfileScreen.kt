f59c518f290995e325e516a5091bdaba0cc6d7d2
YHAWE <ibagares36632@liceo.edu.ph>
2026-09-09 10:37:18 +0800

    Lab Activity 9: Final Submission

--- app/src/main/java/com/example/myapplication/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/ProfileScreen.kt
@@ -23,7 +23,7 @@ fun ProfileScreen(postsVm: PostsViewModel, themeVm: ThemeViewModel) {
     ) {
         Icon(Icons.Default.AccountCircle, null, Modifier.size(96.dp))
         Spacer(Modifier.height(8.dp))
-        Text("Avila Student", style = MaterialTheme.typography.headlineSmall)
+        Text("Hans Jerald Avila", style = MaterialTheme.typography.headlineSmall)
         Text("@avilastudent", style = MaterialTheme.typography.bodyMedium)
         Text("BSIT-3 · Liceo de Cagayan University",
              style = MaterialTheme.typography.bodySmall)
